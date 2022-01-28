49cc315105b6c637d7f151ca3aba7d3945b0e527
Nataliya Ioffe <nataliya.ioffe@rangle.io>
2022-01-27 21:28:20 -0500

    Create topping pages and filter pizza page by topping

--- gatsby/src/components/ToppingsFilter.js
+++ gatsby/src/components/ToppingsFilter.js
@@ -19,7 +19,7 @@ const ToppingsStyles = styled.div`
       background: white;
       padding: 2px 5px;
     }
-    .active {
+    &[aria-current='page'] {
       background: var(--yellow);
     }
   }
@@ -79,6 +79,10 @@ export default function ToppingsFilter() {
   // loop over the list of toppings with counts to display toppings filters
   return (
     <ToppingsStyles>
+      <Link to="/pizzas">
+        <span className="name">All</span>
+        <span className="count">{data.pizzas.nodes.length}</span>
+      </Link>
       {toppingsWithCounts.map((topping) => (
         <Link to={`/topping/${topping.name}`} key={topping.id}>
           <span className="name">{topping.name}</span>
