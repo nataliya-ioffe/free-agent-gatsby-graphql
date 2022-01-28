fee9a24d0105229c21965117a72d114c2c233097
Nataliya Ioffe <nataliya.ioffe@rangle.io>
2022-01-27 22:38:48 -0500

    Source data from an external sample API

--- gatsby/gatsby-node.js
+++ gatsby/gatsby-node.js
@@ -1,8 +1,9 @@
 // From Node
 import path from 'path';
+import fetch from 'isomorphic-fetch';
 
 async function turnPizzasIntoPages({ graphql, actions }) {
-  // Step 1: Get a template for this Pizza page
+  // Step 1: Define a template for this Pizza page
   const pizzaTemplate = path.resolve('./src/templates/PizzaTemplate.js');
 
   // Step 2: Query for the name and slugs of all pizzas
@@ -23,7 +24,6 @@ async function turnPizzasIntoPages({ graphql, actions }) {
 
   // Step 3: Loop over each pizza to create a page for that pizza
   data.pizzas.nodes.forEach((pizza) => {
-    console.log('creating a page for', pizza.name);
     actions.createPage({
       // Choose the URL for the new page being created
       path: `pizza/${pizza.slug.current}`,
@@ -39,12 +39,10 @@ async function turnPizzasIntoPages({ graphql, actions }) {
 }
 
 async function turnToppingsIntoPages({ graphql, actions }) {
-  // Step 1: Get a template for the Topping page
+  // Step 1: Define a template for the topping page
   const toppingTemplate = path.resolve('./src/pages/pizzas.js');
 
-  // Step 2: Query for the name and slugs of all pizzas
-  // Syntax is a bit different here than other queries because we are using the Node API
-  // await graphql(`...`) vs graphql`...`
+  // Step 2: Query for all pizza toppings
   const { data } = await graphql(`
     query {
       toppings: allSanityTopping {
@@ -56,7 +54,7 @@ async function turnToppingsIntoPages({ graphql, actions }) {
     }
   `);
 
-  // Step 3: Loop over each pizza to create a page for that pizza
+  // Step 3: Create a page for each pizza topping
   data.toppings.nodes.forEach((topping) => {
     console.log('Creating a page for', topping.name);
     actions.createPage({
@@ -71,6 +69,44 @@ async function turnToppingsIntoPages({ graphql, actions }) {
       },
     });
   });
+}
+
+// Source data from an external API (a sample beer API)
+// 1. Fetch a list of beers
+// 2. Create a node for each beer
+async function fetchBeersAndTurnIntoNodes({
+  actions,
+  createNodeId,
+  createContentDigest,
+}) {
+  // use fetch import from Node
+  const res = await fetch('https://api.sampleapis.com/beers/ale');
+  const beers = await res.json();
+
+  beers.forEach((beer) => {
+    // // Create metadata for each beer node
+    const nodeMetadata = {
+      // createNodeId from gatsby -
+      id: createNodeId(`beer-${beer.name}`),
+      parent: null,
+      children: [],
+      internal: {
+        type: 'Beer',
+        mediaType: 'application/json',
+        contentDigest: createContentDigest(beer),
+      },
+    };
+
+    actions.createNode({
+      ...beer,
+      ...nodeMetadata,
+    });
+  });
+}
+
+export async function sourceNodes(params) {
+  // Fetch a list of beers and source them into our Gatsby API by creating nodes
+  await Promise.all([fetchBeersAndTurnIntoNodes(params)]);
 }
 
 export async function createPages(params) {
